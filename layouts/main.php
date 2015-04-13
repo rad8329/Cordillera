9ab3bda2f5d0fde8fe6e242a560c3d8eaafcdb0e
Robert Díaz <rad8329@gmail.com>
2015-04-13 14:41:17 -0500

    Fix main layout

--- layouts/main.php
+++ layouts/main.php
@@ -23,20 +23,15 @@ use \cordillera\base\Application;
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $this->getProperty("title", "Cordillera") ?></title>
     <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css"
-          href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css"
-          href="<?php echo Application::getRequest()->base_url ?>/media/css/normalize.css">
-    <link rel="stylesheet" type="text/css"
-          href="<?php echo Application::getRequest()->base_url ?>/media/css/cordillera.css">
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo Application::getRequest()->base_url ?>/media/css/normalize.css">
+    <link href="//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/themes/blue/pace-theme-flash.css" rel="stylesheet"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo Application::getRequest()->base_url ?>/media/css/cordillera.css">
     <link rel="stylesheet" type="text/css" href="<?php echo Application::getRequest()->base_url ?>/media/css/custom.css">
     <script src="//code.jquery.com/jquery-2.1.3.min.js" type="text/javascript"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="<?php echo Application::getRequest()->base_url ?>/media/js/cordillera.js" type="text/javascript"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js"></script>
-    <link href="//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/themes/blue/pace-theme-flash.css"
-          rel="stylesheet"/>
     <script src="<?php echo Application::getRequest()->base_url ?>/media/js/bootstrap-confirmation.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js"></script>
     <?php echo $this->publishRegisteredFiles() ?>
 </head>
 <body>
@@ -44,8 +39,7 @@ use \cordillera\base\Application;
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                 <span class="sr-only"><?php echo Application::getLang()->translate('Toggle navigation') ?></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -72,5 +66,6 @@ use \cordillera\base\Application;
     </div>
 <?php endif ?>
 <?php echo $content ?>
+<script src="<?php echo Application::getRequest()->base_url ?>/media/js/cordillera.js" type="text/javascript"></script>
 </body>
 </html>
